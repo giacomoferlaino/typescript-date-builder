b17503bc11a5151ad373bcff26ba634cb1480ab0
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-09-10 13:31:29 +0200

    feat: Added equals, isAfter, isBefore helper methods

--- src/date-helpers.ts
+++ src/date-helpers.ts
@@ -7,4 +7,16 @@ export class DateHelpers {
   static daysInPreviousMonth(date: Date): number {
     return new Date(date.getFullYear(), date.getMonth(), 0).getDate();
   }
+
+  static equals(firstDate: Date, secondDate: Date): boolean {
+    return firstDate.getTime() == secondDate.getTime();
+  }
+
+  static isAfter(firstDate: Date, secondDate: Date): boolean {
+    return firstDate.getTime() > secondDate.getTime();
+  }
+
+  static isBefore(firstDate: Date, secondDate: Date): boolean {
+    return firstDate.getTime() < secondDate.getTime();
+  }
 }
--- src/spec/date-helpers.spec.ts
+++ src/spec/date-helpers.spec.ts
@@ -17,4 +17,81 @@ describe('class: DateHelpers', () => {
       expect(DateHelpers.daysInPreviousMonth(new Date(2001, 2, 1))).toBe(28);
     });
   });
+
+  describe('method: equals', () => {
+    it('should return true if the two dates are equal', () => {
+      expect(
+        DateHelpers.equals(new Date(2000, 0, 1), new Date(2000, 0, 1))
+      ).toBe(true);
+      expect(
+        DateHelpers.equals(
+          new Date(1999, 11, 31, 0, 0, 0, 1),
+          new Date(1999, 11, 31, 0, 0, 0, 1)
+        )
+      ).toBe(true);
+    });
+    it('should return false if the two dates are not equal', () => {
+      expect(
+        DateHelpers.equals(new Date(2001, 0, 1), new Date(2000, 0, 1))
+      ).toBe(false);
+      expect(
+        DateHelpers.equals(
+          new Date(2000, 0, 1, 0, 0, 0, 1),
+          new Date(2000, 0, 1, 0, 0, 0, 0)
+        )
+      ).toBe(false);
+    });
+  });
+
+  describe('method: isAfter', () => {
+    it('should return true if the first date is later than the second one', () => {
+      expect(
+        DateHelpers.isAfter(new Date(2000, 0, 1), new Date(1999, 0, 1))
+      ).toBe(true);
+      expect(
+        DateHelpers.isAfter(
+          new Date(2000, 0, 2, 0, 0, 0, 1),
+          new Date(2000, 0, 2, 0, 0, 0, 0)
+        )
+      ).toBe(true);
+    });
+
+    it('should return false if the first date is before than the second one', () => {
+      expect(
+        DateHelpers.isAfter(new Date(1999, 0, 1), new Date(2000, 0, 1))
+      ).toBe(false);
+      expect(
+        DateHelpers.isAfter(
+          new Date(2000, 0, 2, 0, 0, 0, 0),
+          new Date(2000, 0, 2, 0, 0, 0, 1)
+        )
+      ).toBe(false);
+    });
+  });
+
+  describe('method: isBefore', () => {
+    it('should return true if the first date is before than the second one', () => {
+      expect(
+        DateHelpers.isBefore(new Date(1999, 0, 1), new Date(2000, 0, 1))
+      ).toBe(true);
+      expect(
+        DateHelpers.isBefore(
+          new Date(2000, 0, 2, 0, 0, 0, 0),
+          new Date(2000, 0, 2, 0, 0, 0, 1)
+        )
+      ).toBe(true);
+    });
+
+    it('should return false if the first date is later than the second one', () => {
+      expect(
+        DateHelpers.isBefore(new Date(2000, 0, 1), new Date(1999, 0, 1))
+      ).toBe(false);
+      expect(
+        DateHelpers.isBefore(
+          new Date(2000, 0, 2, 0, 0, 0, 1),
+          new Date(2000, 0, 2, 0, 0, 0, 0)
+        )
+      ).toBe(false);
+    });
+  });
 });
