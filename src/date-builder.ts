7b6dcf0ec6afab88194d9403e94817a7ef2f547c
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-09-10 12:59:17 +0200

    feat: Added methods to add and subtract months inside DateBuilder

--- src/date-builder.ts
+++ src/date-builder.ts
@@ -1,12 +1,14 @@
+import { DateHelpers } from './date-helpers';
+
 export class DateBuilder {
-  static milliseconds: number = 1000;
-  static seconds: number = 60;
-  static minutes: number = 60;
-  static hours: number = 24;
-  private dayMultipler: number;
+  static readonly milliseconds: number = 1000;
+  static readonly seconds: number = 60;
+  static readonly minutes: number = 60;
+  static readonly hours: number = 24;
+  private readonly millisecondsInADay: number;
 
   constructor(private date: Date) {
-    this.dayMultipler =
+    this.millisecondsInADay =
       DateBuilder.milliseconds *
       DateBuilder.seconds *
       DateBuilder.minutes *
@@ -21,9 +23,43 @@ export class DateBuilder {
     return new DateBuilder(new Date(Date.now()));
   }
 
+  private checkNegativeNumber(value: number): void {
+    if (value < 0) throw new TypeError('Negative number not supported.');
+  }
+
   addDays(days: number): DateBuilder {
-    const daysToAdd: number = days * this.dayMultipler;
+    this.checkNegativeNumber(days);
+    const daysToAdd: number = days * this.millisecondsInADay;
     this.date = new Date(this.date.getTime() + daysToAdd);
+    return this;
+  }
+
+  subtractDays(days: number): DateBuilder {
+    this.checkNegativeNumber(days);
+    const daysToAdd: number = days * this.millisecondsInADay;
+    this.date = new Date(this.date.getTime() - daysToAdd);
+    return this;
+  }
+
+  addMonths(months: number): DateBuilder {
+    this.checkNegativeNumber(months);
+    for (let i: number = 0; i < months; i++) {
+      const daysInCurrentMonth: number = DateHelpers.daysInCurrentMonth(
+        this.date
+      );
+      this.addDays(daysInCurrentMonth);
+    }
+    return this;
+  }
+
+  subtractMonths(months: number): DateBuilder {
+    this.checkNegativeNumber(months);
+    for (let i: number = 0; i < months; i++) {
+      const daysInPreviousMonth: number = DateHelpers.daysInPreviousMonth(
+        this.date
+      );
+      this.subtractDays(daysInPreviousMonth);
+    }
     return this;
   }
 }
--- src/spec/date-builder.spec.ts
+++ src/spec/date-builder.spec.ts
@@ -1,8 +1,11 @@
 import { DateBuilder } from '../date-builder';
 
-const nowDateMock: number = new Date(2000, 0, 1).getTime();
+const nowDateMock: Date = new Date(2000, 0, 1);
 
 describe('class: DateBuilder', () => {
+  const negativeNumberError: TypeError = new TypeError(
+    'Negative number not supported.'
+  );
   let dateBuilder: DateBuilder;
 
   beforeEach(() => {
@@ -23,12 +26,12 @@ describe('class: DateBuilder', () => {
 
   describe('method: today', () => {
     beforeEach(() => {
-      jest.spyOn(Date, 'now').mockReturnValue(nowDateMock);
+      jest.spyOn(Date, 'now').mockReturnValue(nowDateMock.getTime());
       dateBuilder = DateBuilder.today();
     });
 
     it('should create a DateBuilder set to the current date', () => {
-      expect(dateBuilder.getDate()).toEqual(new Date(nowDateMock));
+      expect(dateBuilder.getDate()).toEqual(nowDateMock);
     });
   });
 
@@ -36,25 +39,96 @@ describe('class: DateBuilder', () => {
     let daysToAdd: number;
     let expectedDate: Date;
 
-    beforeEach(() => {
-      daysToAdd = 40;
-      expectedDate = new Date(2000, 1, 10);
-    });
-
     it('should return a DateBuilder instance', () => {
+      daysToAdd = 0;
       expect(dateBuilder.addDays(daysToAdd)).toBeInstanceOf(DateBuilder);
     });
 
-    it('should add the given days to the date in use', () => {
+    it('should add the given number days to the date in use', () => {
+      daysToAdd = 40;
+      expectedDate = new Date(2000, 1, 10);
       dateBuilder = dateBuilder.addDays(daysToAdd);
-      expect(dateBuilder.getDate()).toEqual(expectedDate);
+      expect(dateBuilder.getDate().getTime()).toEqual(expectedDate.getTime());
     });
 
-    it('should also work with negative numbers', () => {
+    it('should throw a TypeError if the number of days is negative (no one likes negativity)', () => {
       daysToAdd = -10;
-      expectedDate = new Date(1999, 11, 22);
-      dateBuilder = dateBuilder.addDays(daysToAdd);
-      expect(dateBuilder.getDate()).toEqual(expectedDate);
+      try {
+        dateBuilder.addDays(daysToAdd);
+      } catch (err) {
+        expect(err).toEqual(negativeNumberError);
+      }
+    });
+  });
+
+  describe('method: subtractDays', () => {
+    let daysToSubtract: number;
+    let expectedDate: Date;
+
+    it('should return a DateBuilder instance', () => {
+      daysToSubtract = 0;
+      expect(dateBuilder.subtractDays(daysToSubtract)).toBeInstanceOf(
+        DateBuilder
+      );
+    });
+
+    it('should subtract the given number days to the date in use', () => {
+      daysToSubtract = 40;
+      expectedDate = new Date(1999, 10, 22);
+      dateBuilder = dateBuilder.subtractDays(daysToSubtract);
+      expect(dateBuilder.getDate().getTime()).toEqual(expectedDate.getTime());
+    });
+
+    it('should throw a TypeError if the number of days is negative (no one likes negativity, again)', () => {
+      daysToSubtract = -10;
+      try {
+        dateBuilder.subtractDays(daysToSubtract);
+      } catch (err) {
+        expect(err).toEqual(negativeNumberError);
+      }
+    });
+  });
+
+  describe('method: addMonths', () => {
+    let monthsToAdd: number;
+    let expectedDate: Date;
+
+    it('should return a DateBuilder instance', () => {
+      expect(dateBuilder.addMonths(monthsToAdd)).toBeInstanceOf(DateBuilder);
+    });
+
+    it('should add the given number of months to the date in use', () => {
+      monthsToAdd = 4;
+      expectedDate = new Date(2000, 4, 1, 1); // 1 hour added because of the timezone "problem"
+      dateBuilder = dateBuilder.addMonths(monthsToAdd);
+      expect(dateBuilder.getDate().getTime()).toEqual(expectedDate.getTime());
+    });
+
+    it('should throw a TypeError if the number of months is negative', () => {
+      monthsToAdd = -10;
+      try {
+        dateBuilder.subtractDays(monthsToAdd);
+      } catch (err) {
+        expect(err).toEqual(negativeNumberError);
+      }
+    });
+  });
+
+  describe('method: subtractMonths', () => {
+    let monthsToSubtract: number;
+    let expectedDate: Date;
+
+    it('should return a DateBuilder instance', () => {
+      expect(dateBuilder.subtractMonths(monthsToSubtract)).toBeInstanceOf(
+        DateBuilder
+      );
+    });
+
+    it('should subtract the given number of months to the date in use', () => {
+      monthsToSubtract = 2;
+      expectedDate = new Date(1999, 10, 1);
+      dateBuilder = dateBuilder.subtractMonths(monthsToSubtract);
+      expect(dateBuilder.getDate().getTime()).toEqual(expectedDate.getTime());
     });
   });
 });
